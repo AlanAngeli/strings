2a374ed5f2f014373d7a70a04aa88fa4908b44cf
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-01-30 22:41:57 -0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,11 @@
+#positivo [012345678]
+
+texto = "Python 3"
+print(texto[7]) # [7] se refere a sétimo caracter do str
+
+#negativos -[987654321]
+
+print(texto[-1])
+
+url = "www.google.com.br/"
+print(url[:-1])
